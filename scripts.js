3d7244a5fe2a5bcb8b762776cb4b44c72bcf50e5
Bunei shadieh <buneishadrack5@gmail.com>
2020-12-10 23:53:14 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+var add=function(number1,number2){
+    return number1+number2;
+}
+var number1=parseInt(prompt("Enter your number"));
+var number2=parseInt(prompt("Enter the other number"));
+var result= add(number1,number2)
+alert ("wow your results is " + result)
+
+//subtraction 
+var subtract=function(number1,number2){
+    return number1-number2;
+}
+var number1=parseInt(prompt("Enter your number"));
+var number2=parseInt(prompt("Enter the other number"));
+var result= subtract(number1,number2);
+alert ("wow your results is " + result);
+
+//multipliaction
+var multiplication=function(number1,number2){
+    return number1*number2;
+}
+var number1=parseInt(prompt("Enter your number"));
+var number2=parseInt(prompt("Enter the other number"));
+var result= multiplication(number1,number2);
+alert ("wow your results is " + result);
+
+//division
+var div=function(number1,number2){
+    return number1/number2;
+};
+var number1=parseInt(prompt("Enter your number"));
+var number2=parseInt(prompt("Enter the other number"));
+var result= div(number1,number2);
+alert ("wow your results is " + result);
